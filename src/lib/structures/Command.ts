831346e724c2e06e303a8b5a304bddb36fbb9bc2
ijsKoud <github@ijskoud.dev>
2022-09-29 21:25:21 +0200

    feat(Command): add options and (un)load functions

--- src/lib/structures/Command.ts
+++ src/lib/structures/Command.ts
@@ -1,9 +1,36 @@
 import type { PentagonClient } from "../Client.js";
 
-export class Command {
+export class Command implements CommandOptions {
 	public name: string;
+	public description: string;
 
-	public constructor(public client: PentagonClient) {
-		this.name = "test"; // TODO: options
+	/** @internal The name of the file associated with the command */
+	public filename: string;
+
+	public constructor(public client: PentagonClient, options: CommandOptions) {
+		this.name = options.name;
+		this.description = options.description;
+		this.filename = __filename;
 	}
+
+	/**
+	 * Runs when the command is loaded
+	 */
+	public load() {
+		// placeholder for possible load function for command
+	}
+
+	/**
+	 * Runs when the command is unloaded
+	 */
+	public unload() {
+		// placeholder for possible unload function for command
+	}
+}
+
+interface CommandOptions {
+	/** The name of the command */
+	name: string;
+	/** A small description about the command */
+	description: string;
 }
