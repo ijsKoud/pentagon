408468376310bc8e754c6d9a8593dcfc7368fc96
ijsKoud <github@ijskoud.dev>
2022-09-29 16:36:19 +0200

    feat: add InteractionHandlerError class

---
+++ src/lib/Errors/InteractionHandlerError.ts
@@ -0,0 +1,11 @@
+export class InteractionHandlerError extends Error {
+	public type: InteractionHandlerErrorType;
+
+	public constructor(errorType: InteractionHandlerErrorType, message?: string, options?: ErrorOptions) {
+		super(message, options);
+		this.name = "InteractionHandlerError";
+		this.type = errorType;
+	}
+}
+
+type InteractionHandlerErrorType = "InvalidDirectory" | "InvalidStructureClass";
