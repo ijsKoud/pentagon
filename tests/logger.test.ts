b3d713d6258a553438ed8fd5a3abc536fb54bb4d
ijsKoud <github@ijskoud.dev>
2022-10-01 15:20:57 +0200

    test: clear mockData after every test

--- tests/logger.test.ts
+++ tests/logger.test.ts
@@ -15,42 +15,56 @@ describe("Testing the logger functions", () => {
 	test("Logger: Trace function", () => {
 		const trace = jest.spyOn(console, "trace").mockImplementation(() => void 0);
 		logger.trace("Hello World");
+
 		expect(trace).toBeCalledWith(`${timestamp}[TRACE] » Hello World`);
+		trace.mockClear();
 	});
 
 	test("Logger: Debug function", () => {
 		const debug = jest.spyOn(console, "debug").mockImplementation(() => void 0);
 		logger.debug("Hello World");
+
 		expect(debug).toBeCalledWith(`${timestamp}[DEBUG] » Hello World`);
+		debug.mockClear();
 	});
 
 	test("Logger: Warn function", () => {
 		const warn = jest.spyOn(console, "warn").mockImplementation(() => void 0);
 		logger.warn("Hello World");
+
 		expect(warn).toBeCalledWith(`${timestamp}[WARN]  » Hello World`);
+		warn.mockClear();
 	});
 
 	test("Logger: Info function", () => {
 		const info = jest.spyOn(console, "info").mockImplementation(() => void 0);
 		logger.info("Hello World");
+
 		expect(info).toBeCalledWith(`${timestamp}[INFO]  » Hello World`);
+		info.mockClear();
 	});
 
 	test("Logger: Error function", () => {
 		const error = jest.spyOn(console, "error").mockImplementation(() => void 0);
 		logger.error("Hello World");
+
 		expect(error).toBeCalledWith(`${timestamp}[ERROR] » Hello World`);
+		error.mockClear();
 	});
 
 	test("Logger: Fatal function", () => {
 		const fatal = jest.spyOn(console, "error").mockImplementation(() => void 0);
 		logger.fatal("Hello World");
+
 		expect(fatal).toBeCalledWith(`${timestamp}[FATAL] » Hello World`);
+		fatal.mockClear();
 	});
 
 	test("Logger: write function", () => {
 		const log = jest.spyOn(console, "log").mockImplementation(() => void 0);
 		logger.write(LogLevel.None, "Hello World");
+
 		expect(log).toBeCalledWith(`${timestamp}Hello World`);
+		log.mockClear();
 	});
 });
