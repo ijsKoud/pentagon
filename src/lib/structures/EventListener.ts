86a09f102f262b73ab4555ef029c8e8ac5f1d4c4
ijsKoud <github@ijskoud.dev>
2022-10-02 10:54:50 +0200

    feat(EventListener): add sub and unsubscribing

--- src/lib/structures/EventListener.ts
+++ src/lib/structures/EventListener.ts
@@ -1,36 +1,58 @@
+import type { Awaitable, ClientEvents } from "discord.js";
+import type EventEmitter from "events";
 import type { PentagonClient } from "../Client.js";
 
-export class EventListener implements CommandOptions {
-	public name: string;
+export class EventListener implements EventListenerOptions {
+	public name: keyof ClientEvents | string;
+	public emitter: EventEmitter;
+	public once?: boolean | undefined;
 
-	/** @internal The name of the file associated with the command */
+	/** @internal The name of the filepath associated with the event */
 	public filepath!: string;
 
-	public constructor(public client: PentagonClient, options: CommandOptions) {
+	public constructor(public client: PentagonClient, options: EventListenerOptions) {
 		this.name = options.name;
+		this.emitter = options.emitter ?? this.client;
+		this.once = options.once;
+	}
+
+	public run<K extends keyof ClientEvents>(...args: ClientEvents[K] | any): Awaitable<void> {
+		void 0; // placeholder
 	}
 
 	/**
 	 * Runs when the command is loaded
 	 * @requires super callback on overwrite
 	 */
-	public load(options: CommandLoadOptions) {
+	public load(options: EventListenerLoadOptions) {
 		this.filepath = options.filepath;
+		this.emitter[this.once ? "once" : "on"](this.name, this.run.bind(this));
 	}
 
 	/**
-	 * Runs when the command is unloaded
+	 * Runs when the Event is unloaded
+	 * @requires super callback on overwrite
 	 */
 	public unload() {
-		// placeholder for possible unload function for command
+		this.emitter.off(this.name, this.run.bind(this));
 	}
 }
 
-export interface CommandOptions {
-	/** The name of the command */
-	name: string;
+export interface EventListenerOptions {
+	/** The name of the event */
+	name: string | keyof ClientEvents;
+	/**
+	 * Whether to run the event once or multiple times after starting the bot
+	 * @default false
+	 */
+	once?: boolean;
+	/**
+	 * The emitter of the event
+	 * @defaults PentagonClient
+	 */
+	emitter?: EventEmitter;
 }
 
-export interface CommandLoadOptions {
+export interface EventListenerLoadOptions {
 	filepath: string;
 }
