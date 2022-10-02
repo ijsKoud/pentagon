655f88fb837a6bd195211b301d74c41cc75cab2a
ijsKoud <github@ijskoud.dev>
2022-10-02 10:44:03 +0200

    feat(EventHandler): add eventhandler stuff

--- src/lib/structures/EventListener.ts
+++ src/lib/structures/EventListener.ts
@@ -1,9 +1,36 @@
 import type { PentagonClient } from "../Client.js";
 
-export class EventListener {
+export class EventListener implements CommandOptions {
 	public name: string;
 
-	public constructor(public client: PentagonClient) {
-		this.name = "test"; // TODO: options
+	/** @internal The name of the file associated with the command */
+	public filepath!: string;
+
+	public constructor(public client: PentagonClient, options: CommandOptions) {
+		this.name = options.name;
 	}
+
+	/**
+	 * Runs when the command is loaded
+	 * @requires super callback on overwrite
+	 */
+	public load(options: CommandLoadOptions) {
+		this.filepath = options.filepath;
+	}
+
+	/**
+	 * Runs when the command is unloaded
+	 */
+	public unload() {
+		// placeholder for possible unload function for command
+	}
+}
+
+export interface CommandOptions {
+	/** The name of the command */
+	name: string;
+}
+
+export interface CommandLoadOptions {
+	filepath: string;
 }
