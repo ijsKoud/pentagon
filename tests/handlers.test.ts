a62628dbdc8bd7b2d9371b6095950d26a2afdf2e
ijsKoud <github@ijskoud.dev>
2022-10-04 15:17:41 +0200

    test: add tests for ErrorHandler

--- tests/handlers.test.ts
+++ tests/handlers.test.ts
@@ -1,5 +1,7 @@
 import { PentagonClient } from "./mocks/ClientMock";
 import { describe, test, expect, vi } from "vitest";
+import { bold, underline } from "colorette";
+import { InteractionReplyOptions, RepliableInteraction } from "discord.js";
 
 describe("test(EventHandler): handling & running functions", () => {
 	const client = new PentagonClient();
@@ -13,5 +15,33 @@ describe("test(EventHandler): handling & running functions", () => {
 		client.emit("test");
 
 		expect(log).toBeCalledWith("test(EventListener): test complete, event was called.");
+	});
+});
+
+describe("test(ErrorHandler): Discord responses & console.log", () => {
+	test("ErrorHandler: handling Discord responses", () => {
+		const client = new PentagonClient({ console });
+		const log = vi.spyOn(console, "error").mockImplementation(() => void 0);
+
+		client.errorHandler.handleError(new Error("This is a mock Error"));
+		expect(log).toBeCalledWith(`[ERROR] » ${bold(underline(`Error(Error)`))}: This is a mock Error`);
+	});
+
+	test("ErrorHandler: handling Discord responses", () => {
+		const mockInteraction = {
+			followUp: (options: InteractionReplyOptions) => new Promise((res) => res(void 0)),
+			isRepliable: () => true,
+			guildId: "123456789012",
+			channelId: "123456789012"
+		} as unknown as RepliableInteraction;
+
+		const client = new PentagonClient();
+		const followUpSpy = vi.spyOn(mockInteraction, "followUp");
+
+		client.errorHandler.handleError(new Error("This is a mock Error"), mockInteraction);
+		expect(followUpSpy).toBeCalledWith({
+			content:
+				"Welcome to our corner of errors, a place you shouldn't come to too often. It is probably not your fault though, something on our side brought you here. Stay safe out there, if this happens again make sure to contact the support team."
+		});
 	});
 });
