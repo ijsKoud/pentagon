c08d7cfca173717e6710d0d9efd0c3bbf7c779d5
ijsKoud <github@ijskoud.dev>
2022-10-07 14:50:53 +0200

    feat(InteractionCreate): add InteractionListener handler

--- src/bot/events/interaction/InteractionCreate.ts
+++ src/bot/events/interaction/InteractionCreate.ts
@@ -1,5 +1,5 @@
 import { bold } from "colorette";
-import type { CommandInteraction, Interaction } from "discord.js";
+import type { CacheType, CommandInteraction, Interaction, MessageComponentInteraction, ModalSubmitInteraction } from "discord.js";
 import { ApplyOptions } from "../../../lib/decorators/StructureDecorators.js";
 import { InteractionHandlerError } from "../../../lib/Errors/InteractionHandlerError.js";
 import { EventListener, EventListenerOptions } from "../../../lib/structures/EventListener.js";
@@ -10,6 +10,7 @@ import { EventListener, EventListenerOptions } from "../../../lib/structures/Eve
 export default class extends EventListener {
 	public run(interaction: Interaction) {
 		if (interaction.isCommand()) return this.commandInteraction(interaction);
+		if (interaction.isMessageComponent() || interaction.isModalSubmit()) return this.customIdTypeInteraction(interaction);
 	}
 
 	public commandInteraction(interaction: CommandInteraction): void {
@@ -21,5 +22,34 @@ export default class extends EventListener {
 			);
 
 		void command._run(interaction);
+	}
+
+	public customIdTypeInteraction(interaction: MessageComponentInteraction<CacheType> | ModalSubmitInteraction<CacheType>) {
+		const CheckFunction = (str: string, strategy: "equal" | "include" | "endsWith" | "startsWith", check?: (str: string) => boolean) => {
+			if (typeof check === "function") return check(interaction.customId);
+			switch (strategy) {
+				case "include":
+					return str.includes(interaction.customId);
+				case "startsWith":
+					return str.startsWith(interaction.customId);
+				case "endsWith":
+					return str.endsWith(interaction.customId);
+				case "equal":
+				default:
+					return str === interaction.customId;
+			}
+		};
+
+		const interactionListener = this.client.interactionHandler.interactions.find((intListener) =>
+			CheckFunction(intListener.name, intListener.strategy, intListener.check)
+		);
+		if (!interactionListener) return;
+		if (
+			(interaction.isMessageComponent() && interaction.componentType !== interactionListener.type) ||
+			interaction.type !== interactionListener.type
+		)
+			return;
+
+		void interactionListener._run(interaction);
 	}
 }
